391776ca9c60ad0db78116cf6355d589887ddc49
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-04-24 22:19:48 -0700

    Refactor Window logic into main class

--- app/src/main/java/ca/fuwafuwa/kaku/Windows/CaptureWindow.java
+++ app/src/main/java/ca/fuwafuwa/kaku/Windows/CaptureWindow.java
@@ -28,16 +28,8 @@ public class CaptureWindow extends Window implements CaptureWindowCallback  {
     private Drawable borderTranslucent;
     private Drawable border9PatchTransparent;
 
-    private int dX;
-    private int dY;
-
-    private long paramUpdateTimer = System.currentTimeMillis();
-    private Point displaySize;
-
     public CaptureWindow(MainService context) {
         super(context);
-
-        displaySize = mContext.getDisplaySize();
 
         windowView = (WindowView) mWindow.findViewById(R.id.capture_window);
         resizeView = (ResizeView) mWindow.findViewById(R.id.resize_box);
@@ -56,59 +48,28 @@ public class CaptureWindow extends Window implements CaptureWindowCallback  {
         tessThread.start();
     }
 
-    public void reInit(){
-        displaySize = mContext.getDisplaySize();
-        fixBoxBounds();
-        mWindowManager.updateViewLayout(mWindow, params);
-    }
-
     @Override
     public boolean onMoveEvent(MotionEvent e) {
-        Log.d(TAG, params.toString());
         setOpacity(e);
+        boolean handled = super.onMoveEvent(e);
         switch (e.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                dX = params.x - (int) e.getRawX();
-                dY = params.y - (int) e.getRawY();
-                return true;
             case MotionEvent.ACTION_UP:
-                fixBoxBounds();
                 mTessThread.runTess(new BoxParams(params.x, params.y + getStatusBarHeight(), params.width, params.height));
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                params.x = dX + (int) e.getRawX();
-                params.y = dY + (int) e.getRawY();
-                fixBoxBounds();
-                mWindowManager.updateViewLayout(mWindow, params);
-                return true;
+                break;
         }
-        return false;
+        return handled;
     }
 
     @Override
     public boolean onResizeEvent(MotionEvent e) {
         setOpacity(e);
+        boolean handled = super.onResizeEvent(e);
         switch (e.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                dX = params.width - (int) e.getRawX();
-                dY = params.height - (int) e.getRawY();
-                return true;
             case MotionEvent.ACTION_UP:
-                fixBoxBounds();
                 mTessThread.runTess(new BoxParams(params.x, params.y + getStatusBarHeight(), params.width, params.height));
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                params.width = dX + (int) e.getRawX();
-                params.height = dY + (int) e.getRawY();
-                fixBoxBounds();
-                long currTime = System.currentTimeMillis();
-                if (currTime - paramUpdateTimer > 50){
-                    paramUpdateTimer = currTime;
-                    mWindowManager.updateViewLayout(mWindow, params);
-                }
-                return true;
+                break;
         }
-        return true;
+        return handled;
     }
 
     public void showLoadingAnimation(){
@@ -146,50 +107,5 @@ public class CaptureWindow extends Window implements CaptureWindowCallback  {
                 windowBox.setBackground(border9PatchTransparent);
                 break;
         }
-    }
-
-    private void fixBoxBounds(){
-        if (params.x < 0){
-            params.x = 0;
-        }
-        else if (params.x + params.width > displaySize.x) {
-            params.x = displaySize.x - params.width;
-        }
-        if (params.y < 0){
-            params.y = 0;
-        }
-        else if (params.y + params.height > displaySize.y) {
-            params.y = displaySize.y - params.height - getStatusBarHeight();
-        }
-        if (params.width > displaySize.x){
-            params.width = displaySize.x;
-        }
-        if (params.height > displaySize.y){
-            params.height = displaySize.y;
-        }
-        if (params.width < 100){
-            params.width = 100;
-        }
-        if (params.height < 100){
-            params.height = 100;
-        }
-    }
-
-    private int getNavigationBarHeight(){
-        int result = 0;
-        int resourceId = mContext.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
-        if (resourceId > 0){
-            result = mContext.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
-
-    private int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = mContext.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
     }
 }
