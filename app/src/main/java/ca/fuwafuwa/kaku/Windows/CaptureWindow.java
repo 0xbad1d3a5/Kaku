92fd50fcabcd9897e4d39fd7e43787d62f5011e1
Shimizoki <Shimizoki@gmail.com>
2017-01-17 18:37:12 -0500

    Removed extra includes

--- app/src/main/java/ca/fuwafuwa/kaku/Windows/CaptureWindow.java
+++ app/src/main/java/ca/fuwafuwa/kaku/Windows/CaptureWindow.java
@@ -1,8 +1,6 @@
 package ca.fuwafuwa.kaku.Windows;
 
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
